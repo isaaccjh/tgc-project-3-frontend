d5d1e555a4bfc420f50cfbb83cecda3be32fe74d
Isaac <isaacforschool05@gmail.com>
2023-05-05 22:34:45 +0800

    started on layout of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,24 @@
 import "../index.css";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import Logo from "../zerek.png"
 
 export default function Navbar() {
     return (<>
-        <nav class="bg-white dark:bg-gray-800 shadow ">
-            <div className="flex p-5 border-2 border-purple-400">
-                <span className="text-2xl">Zerek</span>
-                <div className="flex">
-                    <div className="border-2 border-red-600">
+        <nav class="bg-yellow-800 dark:bg-gray-800 shadow ">
+            <div className="flex p-5 border-2 border-purple-400 align-middle">
+                    <a>Zerek</a>
+                <div className="flex border-2 border-blue-900 ml-2">
+                    <div className="p-2">
                         <Link to="/">Home</Link>
                     </div>
-                    <div className="border-2 border-red-600">
+                    <div className="p-2">
                         <Link to="/">Products</Link>
                     </div>
-                    <div className="border-2 border-red-600">
+                    <div className="p-2">
                         <Link to="/">About Us</Link>
                     </div>
-                    <div className="border-2 border-red-600">
+                    <div className="p-2">
                         <Link to="/">Contact Us</Link>
                     </div>
                 </div>
